c67151f3d930df4fe46c374ec3731fc907d5fed3
Rashmi <rashmi1rathnayake@gmail.com>
2018-12-20 08:03:47 +0530

    enter transaction done

--- routes/transactions.js
+++ routes/transactions.js
@@ -32,6 +32,49 @@ router.get('/my-transactions',verifyToken, (req,res)=>{
 });
 });
 
+router.post('/addnew',function (req,res) {
+  
+  const newtransactiondata = {
+
+      
+      status:1,
+      amountSettled:req.body.amountSettled,
+      trnDate: new Date(),
+      modifiedDate:new Date(),
+      dueDate:req.body.dueDate,
+      trnStatus:req.body.trnStatus,
+      amountPending:req.body.amountPending,
+      remarks:req.body.remarks,
+
+     // userType:req.body.opt,
+      
+      trnDescription:req.body.trnDescription,
+      totalAmount:req.body.totalAmount
+
+
+  };
+  console.log(newtransactiondata);
+
+     // connection.query("INSERT INTO transactionrecord SET ?", newtransactiondata,function (err,result) {
+    
+       // if(result){
+          let sql = "INSERT INTO transactionrecord (status,amountPending, totalAmount, amountSettled,trnStatus, trnDate, modifiedDate,dueDate,trnDescription,remarks) values(?)"
+          let vals = [newtransactiondata.status,newtransactiondata.amountPending,newtransactiondata.totalAmount,newtransactiondata.amountSettled,newtransactiondata.trnStatus,newtransactiondata.trnDate,newtransactiondata.modifiedDate,newtransactiondata.dueDate,newtransactiondata.trnDescription,newtransactiondata.remarks]
+
+          connection.query(sql,[vals], function (err,result){
+                if(err){
+                  //console.log(err);
+                  res.json({state:false,msg:"data not inserted"})
+                }
+                else{
+                  res.json({state:true,msg:"data inserted"});
+
+             //   }
+            //  });
+            }
+            //connection.release();
+          });
+});
 
 
 
